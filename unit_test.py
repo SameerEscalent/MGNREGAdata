54fe5f22c77b5744c5a712bba77f93e941195661
SameerEscalent <sameer.saurabh@escalent.co>
2023-02-03 14:49:57 +0530

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 13:12:42 2023
+
+"""
+# import filecmp
+# import os
+# import json
+# import tempfile
+import unittest
+from mnrega_data_preprocessing import MNREGADataLoader
+
+class TestPreprocess(unittest.TestCase):
+    
+    # def __init__(self):
+    global data
+    loader = MNREGADataLoader()
+    loader.download()
+    data = loader.process()
+        
+    def test_data_headers(self):
+        self.assertEqual(len(data.columns), 8)
+        
+    def test_data_states(self):
+        self.assertEqual(len(list(data['state'].unique())), 34)
+        
+    def test_data_spend(self):
+        self.assertGreaterEqual(data['total_actual_exp'].sum(), 0)
+        
+if __name__ == '__main__':
+    unittest.main()
