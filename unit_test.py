4f0dd12c2481d0b206d09d9f8fad488c4e601d11
SameerEscalent <sameer.saurabh@escalent.co>
2023-02-03 15:07:46 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -3,16 +3,11 @@
 Created on Fri Feb  3 13:12:42 2023
 
 """
-# import filecmp
-# import os
-# import json
-# import tempfile
 import unittest
 from mnrega_data_preprocessing import MNREGADataLoader
 
 class TestPreprocess(unittest.TestCase):
     
-    # def __init__(self):
     global data
     loader = MNREGADataLoader()
     loader.download()
